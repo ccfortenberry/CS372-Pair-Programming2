013d1740063f79ace2817a04675da26ed2c15b41
Jacob <jbutler12@alaska.edu>
2018-02-14 16:31:58 -0900

    Passed test case 5: three numbers comma or newline delimited

--- calculator-tester.cpp
+++ calculator-tester.cpp
@@ -30,3 +30,11 @@ TEST_CASE("Two numbers newline delimimted", "[calculator]") {
 	REQUIRE(add("2098\n1001") == 3099);
 	REQUIRE(add("998\n11") == 1009);
 }
+
+TEST_CASE("Three numbers newline delimimted", "[calculator]") {
+	REQUIRE(add("1,1,1") == 3);
+	REQUIRE(add("3\n0\n5") == 8);
+	REQUIRE(add("17\n105,20") == 142);
+	REQUIRE(add("2098,1001\n1000") == 4099);
+	REQUIRE(add("998\n11,1") == 1010);
+}
